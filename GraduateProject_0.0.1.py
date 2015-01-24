9bde70a9cd9a55c24f55c134d5f22b34bfd5d5f3
xlion10 <heesong90@nate.com>
2015-01-24 22:51:29 +0900

    graph, modeling

--- GraduateProject_0.0.1.py
+++ GraduateProject_0.0.1.py
@@ -33,28 +33,50 @@ def onMouseMove(e):
 	head_bone.setEuler(x, ey, ez)
 viz.callback(viz.MOUSE_MOVE_EVENT, onMouseMove)
 
-def move(location):
+#def move(location):
+#	[x,y,z] = male.getPosition()
+#	[xr,yr,zr] = male.getEuler()
+#	print [xr,yr,zr]
+#	if location == 'forward':
+#		male.setPosition(x-0.01,y,z)
+#		male.state(2)
+#	elif location == 'back':
+#		male.setPosition(x+0.01,y,z)
+#	elif location == 'left':
+#		male.setPosition(x,y,z+0.01)
+#		male.setEuler(xr-0.01,0,0, viz.REL_LOCAL)
+#	elif location == 'right': 
+#		male.setPosition(x,y,z-0.01)
+#		male.setEuler(xr+0.01,0,0, viz.REL_LOCAL)
+
+#vizact.whilekeydown('w', move, 'forward')
+#vizact.whilekeydown('s', move, 'back')
+#vizact.whilekeydown('a', move, 'left')
+#vizact.whilekeydown('d', move, 'right')
+
+########### MOVEMENT FOR TEST ############
+def forward(location):
 	[x,y,z] = male.getPosition()
-	[xr,yr,zr] = male.getEuler()
-	print [xr,yr,zr]
-	if location == 'forward':
-		male.setPosition(x-0.01,y,z)
+	if location == 'w':
+		male.setPosition(x-0.04,y,z)
 		male.state(2)
-	elif location == 'back':
-		male.setPosition(x+0.01,y,z)
-	elif location == 'left':
-		male.setPosition(x,y,z+0.01)
-		male.setEuler(xr-0.01,0,0, viz.REL_LOCAL)
-	elif location == 'right': 
-		male.setPosition(x,y,z-0.01)
-		male.setEuler(xr+0.01,0,0, viz.REL_LOCAL)
+	elif location == 's':
+		male.setPosition(x+0.04,y,z)
+		male.state(2)
+	elif location == 'a':
+		male.setPosition(x,y,z-0.04)
+	else: 
+		male.setPosition(x,y,z+0.04)
 
-vizact.whilekeydown('w', move, 'forward')
-vizact.whilekeydown('s', move, 'back')
-vizact.whilekeydown('a', move, 'left')
-vizact.whilekeydown('d', move, 'right')
+def move(location):
+	vizact.ontimer2(.01,2,forward, location)
 
+vizact.whilekeydown('w', forward, 'w')
+vizact.whilekeydown('s', forward, 's')
+vizact.whilekeydown('a', forward, 'a')
+vizact.whilekeydown('d', forward, 'd')
 
+############################################
 
 
 
@@ -80,6 +102,10 @@ import texture
 
 
 #############################GRAPH##############################
+import graph
+rotate_number = 0
+coord_number = 0
+obj_number = 0
 
 ################################################################
 
@@ -89,8 +115,18 @@ import Timer
 ################################################################
 
 
+############################MODELING############################
+
+import modeling
+
+################################################################
 
 def onCollideBegin(e):
+	###########graph footholds variable#############
+	global rotate_number
+	global coord_number
+	global obj_number
+	
 	###################Texture######################
 	if e.obj2 == texture.texture_foothold_1:
 		texture.change_texture()
@@ -101,11 +137,45 @@ def onCollideBegin(e):
 	elif e.obj2 == texture.texture_foothold_4:
 		texture.show_blind()
 		
+	######################GRAPH#####################	
+	elif e.obj2 == graph.foothold_object_parent:
+		obj_number = 0
+	elif e.obj2 == graph.foothold_object_child:
+		obj_number = 1
+	elif e.obj2 == graph.foothold_rotate_abs: 
+		rotate_number = 0
+	elif e.obj2 == graph.foothold_rotate_rel: 
+		rotate_number = 1
+	elif e.obj2 == graph.foothold_coord_parent: 
+		coord_number = 0
+	elif e.obj2 == graph.foothold_coord_local: 
+		cood_number = 1
+	elif e.obj2 == graph.foothold_rotate_action: 
+		graph.rotate_action(obj_number, rotate_number, coord_number)
+	elif e.obj2 == graph.foothold_link_action:
+		graph.link_action()
+	elif e.obj2 == graph.foothold_change_link:
+		graph.change_link_position()	
+		
+		
+		
 	##################Timer##########################
 	elif e.obj2 == Timer.timer_spin_slow:
 		Timer.spin_slow()
 	elif e.obj2 == Timer.timer_spin_fast:
 		Timer.spin_fast()
+		
+	##################MODELING#####################		
+	elif e.obj2 == modeling.foothold_change_posture:
+		modeling.change_posture()
+	elif e.obj2 == modeling.foothold_move_posture:
+		vizact.ontimer(.01, modeling.move_posture)
+	elif e.obj2 == modeling.foothold_change_state:
+		modeling.change_state()
+	elif e.obj2 == modeling.foothold_blend_state:
+		modeling.blend_state()
+	elif e.obj2 == modeling.foothold_reset_state:
+		modeling.reset_state()
 		
 viz.callback(viz.COLLIDE_BEGIN_EVENT, onCollideBegin)
 
---
+++ modeling.py
@@ -0,0 +1,97 @@
+﻿import viz
+import vizmat
+import vizact
+
+
+male = viz.add('vcc_male.cfg')
+male.setEuler(180,0,0)
+male.setPosition(-40.5,0,7)
+head = male.getBone( 'Bip01 Head' )
+r_upperarm = male.getBone( 'Bip01 R UpperArm' )
+r_forearm = male.getBone('Bip01 R Forearm')
+r_hand = male.getBone('Bip01 R Hand')
+l_upperarm = male.getBone('Bip01 L UpperArm')
+l_forearm = male.getBone('Bip01 L Forearm')
+l_hand = male.getBone('Bip01 L Hand')
+
+head.lock()
+r_upperarm.lock()
+r_forearm.lock()
+r_hand.lock()
+l_upperarm.lock()
+l_forearm.lock()
+l_hand.lock()
+r_upperarm.setEuler(0,0,-30,viz.AVATAR_LOCAL)
+
+
+def change_posture():
+	r_upperarm.setEuler(-5.0, -26.0, 21.0, viz.AVATAR_LOCAL)
+	r_forearm.setEuler(-35.0, -84.0, 28.0, viz.AVATAR_LOCAL)
+	l_upperarm.setEuler(-16.0, -37.0, -14.0, viz.AVATAR_LOCAL)
+	l_forearm.setEuler(33.0, 7.0, 47.0, viz.AVATAR_LOCAL)
+
+count = 0
+flag = 1
+
+def move_posture():
+	global count
+	global flag
+	if count >= 30:
+		flag = 0
+	elif count <= -30:
+		flag = 1
+	if flag == 0:
+		count = count - 1
+	elif flag == 1:
+		count = count + 1
+	
+	r_hand.setEuler(0,0,count, viz.AVATAR_LOCAL)
+	l_hand.setEuler(0,count,0, viz.AVATAR_LOCAL)
+
+
+foothold_change_posture = viz.add('art/wall.ive')
+foothold_change_posture.setEuler(0,90,0)
+foothold_change_posture.setScale(0.5,0.5,1)
+foothold_change_posture.setPosition(-39.5,0,4)
+foothold_change_posture.collideMesh()
+
+foothold_move_posture = viz.add('art/wall.ive')
+foothold_move_posture.setEuler(0,90,0)
+foothold_move_posture.setScale(0.5,0.5,1)
+foothold_move_posture.setPosition(-41.5,0,4)
+foothold_move_posture.collideMesh()
+
+
+female = viz.add('vcc_female.cfg')
+female.setEuler(180,0,0)
+female.setPosition(-45.5,0,7)
+female.state(1)
+
+def change_state():
+	female.clearActions()
+	female.state(5)
+	
+def blend_state():
+	female.blend(4,1,1)
+
+def reset_state():
+	female.reset()
+	female.state(1)
+
+foothold_change_state = viz.add('art/wall.ive')
+foothold_change_state.setEuler(0,90,0)
+foothold_change_state.setScale(0.5,0.5,1)
+foothold_change_state.setPosition(-44,0,4)
+foothold_change_state.collideMesh()
+
+foothold_blend_state = viz.add('art/wall.ive')
+foothold_blend_state.setEuler(0,90,0)
+foothold_blend_state.setScale(0.5,0.5,1)
+foothold_blend_state.setPosition(-45.5,0,4)
+foothold_blend_state.collideMesh()
+
+foothold_reset_state = viz.add('art/wall.ive')
+foothold_reset_state.setEuler(0,90,0)
+foothold_reset_state.setScale(0.5,0.5,1)
+foothold_reset_state.setPosition(-47,0,4)
+foothold_reset_state.collideMesh()
