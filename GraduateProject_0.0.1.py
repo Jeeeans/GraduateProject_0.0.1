11c586045ea3e5b14cdd2e6c36bfcb686d063488
Sungjun Jin <entelechy39@gmail.com>
2015-01-24 14:14:19 +0900

    Add Texture

--- GraduateProject_0.0.1.py
+++ GraduateProject_0.0.1.py
@@ -1,16 +1,22 @@
 ﻿import viz
 viz.go()
 
-
+#########################Make Background######################
 ground = viz.add('art/sphere_ground3.ive')
+ground.collidePlane(0,1,0,0)
 env = viz.add(viz.ENVIRONMENT_MAP,'sky.jpg')
 
 dome = viz.add('skydome.dlc')
 dome.texture(env)
 
+
+#######################Make Avatar#####################
 male = viz.add('vcc_male.cfg')
 male.setPosition([1,0,0])
 male.setEuler(-90,0,0)
+male.collideMesh()
+#male.enable(viz.PHYSICS)
+male.enable(viz.COLLIDE_NOTIFY)
 
 head_bone = male.getBone('Bip01 Head')
 head_bone.lock()
@@ -24,32 +30,76 @@ def onMouseMove(e):
 		ey = y +(e.dx*0.2)
 	if z - (e.dy*0.5) >-30 and z - (e.dy*0.5) < 20 :
 		ez = z - (e.dy*0.2)
-		
 	head_bone.setEuler(x, ey, ez)
 viz.callback(viz.MOUSE_MOVE_EVENT, onMouseMove)
 
 def forward(location):
 	[x,y,z] = male.getPosition()
-	if location == 'w':
+	def turn_left():
+		vizact.sequence(vizact.turn(1))
+	def turn_right():
+		vizact.turn(-1)
+	if location == 'forward':
 		male.setPosition(x-0.01,y,z)
 		male.state(2)
-	elif location == 's':
+	elif location == 'back':
 		male.setPosition(x+0.01,y,z)
-		male.state(2)
-	elif location == 'a':
-		male.setPosition(x,y,z-0.01)
-	else:
-		male.setPosition(x,y,z+0.01)
+	elif location == 'left':
+		male.addAction(turn_left)
+	elif location == 'right': 
+		male.addAction(turn_right)
 
 def move(location):
 	vizact.ontimer2(.01,2,forward, location)
 
-vizact.onkeydown('w', move, 'w')
-vizact.onkeydown('s', move, 's')
-vizact.onkeydown('a', move, 'a')
-vizact.onkeydown('d', move, 'd')
+vizact.onkeydown('w', move, 'forward')
+vizact.onkeydown('s', move, 'back')
+vizact.onkeydown('a', move, 'left')
+vizact.onkeydown('d', move, 'right')
+
+
+
+
+
+foothold = viz.add('art/wall.ive')
+foothold.setEuler(0,90,0)
+foothold.setScale(0.5,0.5,1)
+foothold.collideMesh()
+#foothold.enable(viz.PHYSICS)
+
 
 
 #vizact.onkeyup('w', male.stopAnimation, 2, 0)
 #view_link = viz.link(head_bone, viz.MainView)
 #viz.eyeheight(0)
+
+
+#############################TEXTURE############################
+import texture
+
+################################################################
+
+
+
+
+#############################GRAPH##############################
+
+################################################################
+
+
+
+
+
+def onCollideBegin(e):
+	if e.obj2 == texture.texture_foothold_1:
+		texture.change_texture()
+	elif e.obj2 == texture.texture_foothold_2:
+		texture.wrap_repeat()
+	elif e.obj2 == texture.texture_foothold_3:
+		texture.show_move()
+	elif e.obj2 == texture.texture_foothold_4:
+		texture.show_blind()
+		
+viz.callback(viz.COLLIDE_BEGIN_EVENT, onCollideBegin)
+
+viz.phys.enable()
---
+++ texture.py
@@ -0,0 +1,71 @@
+﻿import viz
+import vizmat
+import vizact
+
+model = viz.add('art/window.ive')
+window = model.getChild('glass')
+wall = viz.addTexture('art/stone wall.jpg')
+clouds = viz.addTexture('art/tileclouds.jpg')
+moon = viz.addTexture('art/full moon.jpg')
+matrix = vizmat.Transform()
+blend = 0.65
+model.setPosition(-3,0,5)
+
+
+###############Changing Texture##############
+texture_foothold_1 = viz.add('art/wall.ive')
+texture_foothold_1.setEuler(0,90,0)
+texture_foothold_1.setScale(0.5,0.5,1)
+texture_foothold_1.setPosition(-1.5,0,4)
+texture_foothold_1.collideMesh()
+
+def change_texture():
+	window.texture(clouds, '', 1)
+	window.texture(moon, '', 0)
+	window.texblend(0.65, '', 1)
+
+############Texture Copy################
+texture_foothold_2 = viz.add('art/wall.ive')
+texture_foothold_2.setEuler(0,90,0)
+texture_foothold_2.setScale(0.5,0.5,1)
+texture_foothold_2.setPosition(-2.5,0,4)
+texture_foothold_2.collideMesh()
+
+def wrap_repeat():
+	matrix.setScale(2, 2)
+	window.texmat(matrix, '', 0)
+	moon.wrap(viz.WRAP_S, viz.REPEAT)
+	moon.wrap(viz.WRAP_T, viz.REPEAT)
+	clouds.wrap(viz.WRAP_S, viz.REPEAT)
+	clouds.wrap(viz.WRAP_T, viz.REPEAT)
+
+##########moving clouds############
+texture_foothold_3 = viz.add('art/wall.ive')
+texture_foothold_3.setEuler(0,90,0)
+texture_foothold_3.setScale(0.5,0.5,1)
+texture_foothold_3.setPosition(-3.5,0,4)
+texture_foothold_3.collideMesh()
+
+def show_move():
+	def move_clouds():
+		matrix.postTrans(0.001, 0.001, 0)
+		window.texmat(matrix,'',1)
+	vizact.ontimer(.005, move_clouds)
+
+##########Blinding Moon##############
+texture_foothold_4 = viz.add('art/wall.ive')
+texture_foothold_4.setEuler(0,90,0)
+texture_foothold_4.setScale(0.5,0.5,1)
+texture_foothold_4.setPosition(-4.5,0,4)
+texture_foothold_4.collideMesh()
+
+def show_blind():
+	def blind_moon():
+		global blend
+		blend = blend - 0.025
+		window.texblend(blend, '', 1)
+	vizact.ontimer2(0.1, 26, blind_moon)
+	
+	
+
+	
